2645504a1de93211f312dcc688c57600f1f79ba4
rabiaa325 <105245241+rabiaa325@users.noreply.github.com>
2022-11-01 23:55:17 +0300

    Add files via upload

---
+++ Goruntu_isleme_inverting/main.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2 as cv
+
+
+def Image_Inversion(Image):
+    Height = Image.shape[0]
+    Width = Image.shape[1]
+    Channels = Image.shape[2]
+
+    Size = (Height, Width, Channels)
+
+    New_Image = np.zeros(Size, np.uint8)
+
+    for x in range(0, Height):
+        for y in range(0, Width):
+            for c in range(0, Channels):
+                New_Image[x, y, c] = 255 - Image[x, y, c]
+    return New_Image
+
+
+def main():
+    Input_Image = cv.imread("C:\\Users\Bilgisayar\PycharmProjects\Goruntu_isleme_inverting\\Isvicre.jpg")
+    Inverted_Image = Image_Inversion(Input_Image)
+
+    cv.imwrite("Inverted_Image.png", Inverted_Image)
+    input("Please Enter to Continue...")
+
+
+if __name__ == '__main__':
+    main()
+
